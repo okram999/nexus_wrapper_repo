c67040e245e6a8cc654c9a6d125f6e2aaed3ead9
Niristotle Okram <nirish.okram@gmail.com>
2015-11-29 17:50:02 -0800

    cleans up the knife rb file

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -2,10 +2,6 @@ node_path     "nodes"
 role_path     "roles"
 environments_path "environments"
 data_bag_path "data_bags"
-#bootstrap_proxy  "http://web-proxy.usa.hp.com:8080"
-#http_proxy "http://web-proxy.usa.hp.com:8080"
-#https_proxy "http://web-proxy.usa.hp.com:8080"
-#encrypted_data_bag_secret "/home/chef/keys/123hp_utils_databag_secret.key"
-#knife[:secret_file] =  "../../keys/123hp_utils_databag_secret.key"
+
 
 
