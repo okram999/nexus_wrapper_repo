23458ef490975bf9959874446c3672a156c9d6cd
Niristotle Okram <nirish.okram@gmail.com>
2015-11-29 17:13:42 -0800

    adds the source dependency

--- cookbooks/nexus_wrapper/metadata.rb
+++ cookbooks/nexus_wrapper/metadata.rb
@@ -1,7 +1,9 @@
 name 'nexus_wrapper'
 maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer_email 'nirish.okram@gmail.com'
 license 'all_rights'
 description 'Installs/Configures nexus_wrapper'
 long_description 'Installs/Configures nexus_wrapper'
-version '0.1.0'
+version '0.1.1'
+
+depends "nexus"
